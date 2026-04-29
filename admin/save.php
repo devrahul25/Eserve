0c9954ae84258ee6ff4b220b682ad39c10b97508
devrahul25 <rahulgoyal90000@gmail.com>
2026-04-29 12:03:14 +0530

    Restore data-edit attributes to index.html and update save.php logic

--- admin/save.php
+++ admin/save.php
@@ -52,6 +52,94 @@ clearstatcache(true, $CONTENT_PATH);
 // Bump mtime explicitly so any reverse-proxy that uses Last-Modified sees the change.
 @touch($CONTENT_PATH);
 
+// ---- STATIC SITE GENERATION ----
+// The user wants the HTML files to be permanently overwritten with the new content.
+function update_html_files($data, $base_dir) {
+    $files = array_merge(glob($base_dir . '/*.html'), glob($base_dir . '/*/*.html'));
+    
+    foreach ($files as $file) {
+        if (strpos($file, '/admin/') !== false) continue;
+        
+        $html = file_get_contents($file);
+        $changed = false;
+        
+        // 1. Regular data-edit
+        $html = preg_replace_callback(
+            '/(<([a-zA-Z0-9]+)[^>]*data-edit="([^"]+)"[^>]*>)(.*?)(<\/\2>)/is',
+            function($m) use ($data, &$changed) {
+                $opening = $m[1];
+                $key = $m[3];
+                $closing = $m[5];
+                
+                $parts = explode('.', $key);
+                $val = $data;
+                foreach ($parts as $p) {
+                    if (!isset($val[$p])) return $m[0];
+                    $val = $val[$p];
+                }
+                
+                if (is_scalar($val)) {
+                    $changed = true;
+                    // If it has data-edit-html, don't htmlspecialchars
+                    if (stripos($opening, 'data-edit-html') !== false) {
+                        return $opening . $val . $closing;
+                    }
+                    return $opening . htmlspecialchars($val) . $closing;
+                }
+                return $m[0];
+            },
+            $html
+        );
+        
+        // 2. data-edit on inputs/textareas
+        $html = preg_replace_callback(
+            '/(<input[^>]*data-edit="([^"]+)"[^>]*value=")([^"]*)("[^>]*>)/is',
+            function($m) use ($data, &$changed) {
+                $key = $m[2];
+                $parts = explode('.', $key);
+                $val = $data;
+                foreach ($parts as $p) {
+                    if (!isset($val[$p])) return $m[0];
+                    $val = $val[$p];
+                }
+                if (is_scalar($val)) {
+                    $changed = true;
+                    return $m[1] . htmlspecialchars($val) . $m[4];
+                }
+                return $m[0];
+            },
+            $html
+        );
+        
+        // 3. data-edit-href
+        $html = preg_replace_callback(
+            '/(<a[^>]*data-edit-href="([^"]+)"[^>]*href=")([^"]*)("[^>]*>)/is',
+            function($m) use ($data, &$changed) {
+                $key = $m[2];
+                $parts = explode('.', $key);
+                $val = $data;
+                foreach ($parts as $p) {
+                    if (!isset($val[$p])) return $m[0];
+                    $val = $val[$p];
+                }
+                if (is_scalar($val)) {
+                    $changed = true;
+                    preg_match('/data-edit-prefix="([^"]*)"/i', $m[1].$m[4], $pref_m);
+                    $prefix = $pref_m ? $pref_m[1] : '';
+                    return $m[1] . htmlspecialchars($prefix . $val) . $m[4];
+                }
+                return $m[0];
+            },
+            $html
+        );
+
+        if ($changed) {
+            file_put_contents($file, $html);
+        }
+    }
+}
+update_html_files($data, dirname($CONTENT_PATH));
+
 send_json([
     'ok'      => true,
     'bytes'   => $bytes,
